0706c2b72c612e10f6bdee4657ce6a605eb2de02
Luvumbu <luvumbu.n@gmail.com>
2020-07-20 16:37:00 +0200

    ajout des effet des mouvements

--- vue.js
+++ vue.js
@@ -25,31 +25,83 @@ var section = new Vue({
   data: {
     section_source:"src/img/section/photoDefile1.png",
     img_section:"img_section",
+    img_section_value:false,
     img_section_class:"img_section_class_1",
     img_section_text:"Colorez vos souvenirs de voyage",
     img_section_vide:"img_section_vide",
     cercl_section1_parrent:"cercl_section1_parrent",
-    cercl_section1:"cercl_section1",
-    cercl_section2:"cercl_section2",
-    cercl_section3:"cercl_section3"  ,
+    cercl_section1:"cercl_section_color1",
+    cercl_section2:"cercl_section_color2",
+    cercl_section3:"cercl_section_color2",
     message:""  
   } , 
   methods:{
-    ok:function() {
-       this.img_section="img_section2";
+
+    cercl_section_all : function() {
+
+      this.cercl_section1="cercl_section_color2";
+      this.cercl_section2="cercl_section_color2";
+      this.cercl_section3="cercl_section_color2";
+
+    },
+    cercl_section1_1:function() {
+       this.img_section="img_section";
+       this.img_section_value=true;
+       this.cercl_section_all();
+       this.cercl_section1="cercl_section_color1";
+
     } ,
+    cercl_section2_2:function() {
+      this.img_section="img_section2";
+      this.img_section_value=true;
+      this.cercl_section_all();
+      this.cercl_section2="cercl_section_color1";
+   } ,
+   cercl_section3_3:function() {
+    this.img_section="img_section3";
+    this.img_section_value=true;
+    this.cercl_section_all();
+    this.cercl_section3="cercl_section_color1";
+ } ,
     callFunction: function () {
       var v = this;
-      setInterval(function () {
-          
+      setInterval(function () {          
          console.log(v.img_section);
-         if(v.img_section=="img_section"){
-          v.img_section = "img_section2";
-         }
-         else {
-          v.img_section = "img_section";
-         }
-      }, 2000);
+
+
+
+
+         switch(v.img_section) {
+          case "img_section":
+            
+            if(v.img_section_value==false){
+              v.img_section="img_section2";
+              v.cercl_section_all();
+              v.cercl_section2="cercl_section_color1";
+            }
+            break;
+          case "img_section2":
+            if(v.img_section_value==false){
+              v.img_section="img_section3";
+              v.cercl_section_all();
+
+              v.cercl_section3="cercl_section_color1";
+            }           
+            break;
+            case "img_section3":
+              if(v.img_section_value==false){
+                v.img_section="img_section";
+              v.cercl_section_all();
+
+                v.cercl_section1="cercl_section_color1";
+              }
+              break;
+          default:
+       
+        }
+    
+         v.img_section_value=false;
+      }, 4000);
       
   }
   },
